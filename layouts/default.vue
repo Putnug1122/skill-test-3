734dda604ac4bd2e14a8a1e5b1c6b999be741781
deta <deta@pop-os.localdomain>
2021-12-08 19:16:38 +0800

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div class="main-wrapper">
+      <div class="navbar-bg"></div>
+      <partials-navbar />
+      <partials-sidebar />
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="section-body">
+            <Nuxt />
+          </div>
+        </section>
+      </div>
+      <partials-footer />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
